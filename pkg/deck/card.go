8fb466e3360fa2b7a19c9b9a1663ebfa77b97e7e
Jayden Teoh <t3ohjingxiang@gmail.com>
2022-12-26 02:05:13 +0800

    first commit

---
+++ pkg/deck/card.go
@@ -0,0 +1,144 @@
+//go:generate stringer -type=Suit,Rank
+
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+var suits = [...]Suit{Spade, Diamond, Club, Heart} //ellipsis sets the max size
+
+type Rank uint8
+
+const (
+	_ Rank = iota //iota creates incrementing constants (_ skips the first value of 0)
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
+
+// Prints out the rank and suit of a card
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+// Creates a new deck of cards with functional options
+func New(opts ...func([]Card) []Card) []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+	for _, opts := range opts {
+		cards = opts(cards)
+	}
+	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+var shuffleRand = rand.New(rand.NewSource(time.Now().Unix()))
+
+// Shuffle a deck of cards
+func Shuffle(cards []Card) []Card {
+	ret := make([]Card, len(cards))
+	perm := shuffleRand.Perm(len(cards))
+	for i, j := range perm {
+		ret[i] = cards[j]
+	}
+	return ret
+}
+
+// Returns a function that adds inputted number of Joker cards into the deck
+func Jokers(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Rank: Rank(i),
+				Suit: Joker,
+			})
+		}
+		return cards
+	}
+}
+
+// Filter out cards in deck that returns true when passed into inputted function
+func Filter(f func(card Card) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for _, c := range cards {
+			if !f(c) {
+				ret = append(ret, c)
+			}
+		}
+		return ret
+	}
+}
+
+// Create a specified number of duplicated decks of cards
+func Deck(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var ret []Card
+		for i := 0; i < n; i++ {
+			ret = append(ret, cards...)
+		}
+		return ret
+	}
+}
